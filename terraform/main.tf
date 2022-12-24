29d3129cb45403e53afda44ff1cd1c1412cb6205
bhuwanonline <bhuwanonline@gmail.com>
2022-12-24 17:42:07 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,9 +13,22 @@ terraform {
     access_key           = ""
   }
 }
+locals {
+  tags = {
+    tier        = "${var.tier}"
+    deployment  = "${var.deployment}"
+  }
+}
 module "resource_group" {
   source               = "../../modules/resource_group"
   resource_group       = "${var.resource_group}"
   location             = "${var.location}"
 }
-# Reference the AppService Module here.
+module "appservice" {
+  source           = "../../modules/appservice"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "AppService"
+  resource_group   = "${module.resource_group.resource_group_name}"
+  tags             = "${local.tags}"
+}
